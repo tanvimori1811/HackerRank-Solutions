cec1f97c5405032492d37c04046f6f404913ece3
tanvimori1811 <tanvimori70454091@gmail.com>
2025-10-21 11:30:41 +0530

    Remove previous hard folder

---
+++ easy/java/04_DataTypes.java
@@ -0,0 +1,2 @@
+
+import java.util.Scanner; public class Solution { public static void main(String[] args) { Scanner scanner = new Scanner(System.in); int myInt = scanner.nextInt(); double myDouble = scanner.nextDouble(); scanner.nextLine(); String myString = scanner.nextLine(); System.out.println("String: " + myString); System.out.println("Double: " + myDouble); System.out.println("Int: " + myInt); scanner.close(); } }
